27c62402c17d6013dba2956d3b2ae829bce09bcb
AIRGG <>
2022-06-17 23:00:15 +0700

    init flutter

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Project\__INCLUDE__\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\AIRGG\Desktop\Lomba\PKM\__exp__\android_telecare_pkm"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
